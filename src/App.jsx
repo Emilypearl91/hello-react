186f3855b030ba1f4e876723c92ffaaba562496d
Emily Pearl <Emilypearl9193@gmail.com>
2024-04-19 14:03:00 -0600

    components and props

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* user component with props */}
+    <User name='emily' age={20} email='emily@email.com'/>
     </>
-  )
-}
+  );
+};
+// js props object
+const props = {
+  name:'emily',
+  age:20 ,
+  email: 'emily@email.com'
 
+};
+// component must have a capital letter 
+// pass props object as a parameter 
+function User(props){
+
+  return (
+<div>
+  <h1>{props.name}</h1>
+  <h1>{props.age}</h1>
+  <h1>{props.email}</h1>
+</div>
+);
+};
 export default App
