ef6d8ddb6a4c30c5de2a840b014c17796295b131
Emily Pearl <Emilypearl9193@gmail.com>
2024-04-20 16:53:40 -0600

    ternary operators with css

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { User } from './User'
 
 function App() {
-
+ const isPink = true;
+ const users = [
+  {name:'jaclyn', age:21},
+  {name:'aelin', age:3}
+];
   return (
     <>
-    {/* user component with props */}
-    <User name='emily' age={20} email='emily@email.com'/>
+    {/* used map function to display each user  */}
+    {users.map((user,key) => {
+      return <User name={user.name} age={user.age} key={key}/>
+})}
+ {/* using inline css and the isPink variable to change the color of the text*/}
+    <h1 style={{color: isPink ? 'pink' : 'blue'}}>ternary operators and css</h1>
     </>
   );
 };
-// js props object
-const props = {
-  name:'emily',
-  age:20 ,
-  email: 'emily@email.com'
 
-};
-// component must have a capital letter 
-// pass props object as a parameter 
-function User(props){
-
+const User = (props) => {
   return (
-<div>
-  <h1>{props.name}</h1>
-  <h1>{props.age}</h1>
-  <h1>{props.email}</h1>
-</div>
-);
-};
+    <div>
+      {props.name} {props.age}
+    </div>
+  )
+}
+
 export default App
