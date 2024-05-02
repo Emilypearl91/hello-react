c8b47e13616013255082eef17d4b2624ad71ee05
Emily Pearl <Emilypearl9193@gmail.com>
2024-05-02 15:19:48 -0600

    fetching data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
-import { useState } from 'react'
+import { useState,} from 'react'
 import './App.css'
 
 function App() {
-// use state hook notifies react that there is a change and trigger a re render
-// set count function allows for the change a]of count value
-const [count, setCount] = useState(0);
-
-// increase function 
-const increase = () => {
-  // call set count function to increase count by 1 
-  setCount(count + 1)
-}
+fetch('https://catfact.ninja/fact')
+.then((res) => res.json())
+.then((data) => {
+console.log(data);
+});
   return (
     <>
-    <div>
-      {count}
-      <button onClick={increase}>increase</button>
-
-    </div>
+  
     </>
   );
 };
